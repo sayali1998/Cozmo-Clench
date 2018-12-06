889400226bd755949dc76235c4bfc8b4a065baec
sayali1998 <sayalipatil631@gmail.com>
2018-12-07 00:02:55 +0530

    Bot Code

--- Bluetooth_module_test/Bluetooth_module_test.ino
+++ Bluetooth_module_test/Bluetooth_module_test.ino
@@ -1,9 +1,37 @@
 char data = 0;            //Variable for storing received data
+
+int rightforward=3;
+int rightbackward=4;
+int leftforward=5;
+int leftbackward=6
+
 void setup()
 {
     Serial.begin(9600);   //Sets the baud for serial data transmission                               
-    //pinMode(13, OUTPUT);  //Sets digital pin 13 as output pin
+    pinMode(rightforward,OUPUT);
+    pinMode(rightbackward,OUPUT);
+    pinMode(leftforward,OUPUT);
+    pinMode(leftbackward,OUPUT);
+    digitalWrite(rightforward,LOW);
+    digitalWrite(rightbackward,LOW);
+    digitalWrite(leftforward,LOW);
+    digitalWrite(leftbackward,LOW);
 }
+
+
+
+void releaseBlock()
+{
+  
+  }
+
+
+void pickBlock()
+{
+  
+  }
+
+
 void loop()
 {
    if(Serial.available() > 0)      // Send data only when you receive data:
@@ -11,10 +39,73 @@ void loop()
       data = Serial.read();        //Read the incoming data & store into data
       Serial.print(data);          //Print Value inside data in Serial monitor
       Serial.print("\n");        
-      if(data == '1')              // Checks whether value of data is equal to 1
-         digitalWrite(13, HIGH);   //If value is 1 then LED turns ON
-      else if(data == '0')         //  Checks whether value of data is equal to 0
-         digitalWrite(13, LOW);    //If value is 0 then LED turns OFF
+      switch(data)
+      {
+        case '1'://Forward
+                {
+                  digitalWrite(rightforward,HIGH);
+                  digitalWrite(leftforward,HIGH);
+                  digitalWrite(rightbackward,LOW);
+                  digitalWrite(leftbackward,LOW);
+                  break;
+                  }
+        case '2'://Backward
+                {
+                  digitalWrite(rightforward,LOW);
+                  digitalWrite(leftforward,LOW);
+                  digitalWrite(rightbackward,HIGH);
+                  digitalWrite(leftbackward,HIGH);
+                  break;
+                  }
+        case '3'://HardLeft
+                {
+                  digitalWrite(rightforward,HIGH);
+                  digitalWrite(leftforward,LOW);
+                  digitalWrite(rightbackward,LOW);
+                  digitalWrite(leftbackward,HIGH);
+                  break;
+                  }
+        case '4'://HardRight
+                {
+                  digitalWrite(rightforward,LOW);
+                  digitalWrite(leftforward,HIGH);
+                  digitalWrite(rightbackward,HIGH);
+                  digitalWrite(leftbackward,LOW);
+                  break;
+                  }
+        case '5'://SoftLeft
+                {
+                  digitalWrite(rightforward,HIGH);
+                  digitalWrite(leftforward,LOW);
+                  digitalWrite(rightbackward,LOW);
+                  digitalWrite(leftbackward,LOW);
+                  break;
+                  }
+        case '6'://SoftRight   
+                 {
+                  digitalWrite(rightforward,LOW);
+                  digitalWrite(leftforward,HIGH);
+                  digitalWrite(rightbackward,LOW);
+                  digitalWrite(leftbackward,LOW);
+                  break;
+                  }
+        case '7'://Stop
+                 {
+                  digitalWrite(rightforward,LOW);
+                  digitalWrite(leftforward,LOW);
+                  digitalWrite(rightbackward,LOW);
+                  digitalWrite(leftbackward,LOW);
+                  break;
+                  }     
+        }
+        if(data=='8')
+        {
+          releaseBlock();
+          }
+        if(data=='9')
+        {
+          pickBlock();
+          }
    }
 }
 
